217b5dba2350abf41068a6d6ddea994b54c5669e
Pulkit Sachdeva <sachdeva.pulkit2599@gmail.com>
2023-08-19 22:54:36 +0530

    created contact section and added social media links.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css';
+import { TextField } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+import Button from '@mui/material/Button';
+import contactUs from '../../images/contactUs.jpg';
+import { Link } from 'react-router-dom';
 
 function Contact() {
+
+    const [message, setMessage] = useState("");
+
     return (
         <div className='myContact'>
-            <div className="contact-container">
-                contact Me
+            <h1 className='text-center'>Contact <span className='purple'>Me</span></h1>
+            <div>
+                <div className="contact-container">
+                    <Link to="mailto:sachdeva.pulkit2599@gmail.com">
+                        <img src={contactUs} alt="" />
+                    </Link>
+                    <form onSubmit={(e) => { e.preventDefault(); setMessage("I'll get back to you soon."); }}>
+                        <TextField
+                            required
+                            variant="outlined"
+                            type='text'
+                            id="outlined"
+                            label="Name"
+                            fullWidth
+                            sx={{ maxWidth: "400px", margin: "15px 0px" }}
+                        />
+                        <TextField
+                            variant="outlined"
+                            type='email'
+                            id="outlined"
+                            label="Email"
+                            fullWidth
+                            required
+                            sx={{ maxWidth: "400px", margin: "15px 0px" }}
+                        />
+                        <TextField
+                            required
+                            variant="outlined"
+                            type='text'
+                            id="outlined"
+                            label="Message"
+                            fullWidth
+                            sx={{ maxWidth: "400px", margin: "15px 0px" }}
+
+                        />
+                        <Button variant="contained" endIcon={<SendIcon />} color='success' type='submit'>
+                            Send
+                        </Button>
+                        <div className="text-success">{message}</div>
+                    </form>
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import keepNotes from '../../images/banner.jpg';
 import './Home.css'
 import Typewriter from 'typewriter-effect';
 import { MDBIcon } from 'mdb-react-ui-kit';
+import { Link } from 'react-router-dom';
 
 function Home({scrollToElement}) {
 
@@ -14,7 +15,7 @@ function Home({scrollToElement}) {
                     <div>and I am a <span className="purple mx-2">
                         <Typewriter
                             options={{
-                                strings: ['Programmar', 'Web Developer', 'Frontend Developer'],
+                                strings: ['Programmar', 'MERN Developer', 'Frontend Developer'],
                                 autoStart: true,
                                 loop: true,
                                 pauseFor: 1500,
@@ -27,22 +28,22 @@ function Home({scrollToElement}) {
                         various languages like C,C++,Javascript as well as various frameworks like Reactjs, Bootstrap.
                     </div>
                     <div className='cv'>
-                        <button className="btn myBtn me-4">Download CV</button>
+                        <Link to="https://drive.google.com/u/0/uc?id=1xy-HWFiVgcMGdLRAHr0Rp0lJzb6hMtIM&export=download" download={"Pulkit_Resume"}><button className="btn myBtn me-4">Download CV</button></Link>
                         <button className="btn myBtn" onClick={()=>{scrollToElement('.myProjects')}}>Projects</button>
                     </div>
                 </div>
                 <div className='imageSet'>
                     <img src={keepNotes} alt="" />
                     <div className="social-media">
-                        <a href="/">
+                        <Link to="https://www.linkedin.com/in/pulkit-sachdeva-8a72a7121" target='_blank'>
                             <MDBIcon fab icon="linkedin-in" size="2x" />
-                        </a>
-                        <a href="/">
+                        </Link>
+                        <Link to="https://github.com/Devil1205" target='_blank'>
                             <MDBIcon fab icon="github" size="2x" />
-                        </a>
-                        <a href="/">
+                        </Link>
+                        <Link to="https://www.instagram.com/_im_pulkit" target='_blank'>
                             <MDBIcon fab icon="instagram" size="2x" />
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
